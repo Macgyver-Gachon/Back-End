9335f6ff5baeb85d4bd25612e975711046b9c584
hayoon45 <48703206+hayoon45@users.noreply.github.com>
2022-05-20 21:50:08 +0900

    CORS

---
+++ src/main/java/com/project2022/macgyver/controller/BookmarkApiController.java
@@ -0,0 +1,10 @@
+package com.project2022.macgyver.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RequiredArgsConstructor
+//@RestController
+public class BookmarkApiController {
+
+}
